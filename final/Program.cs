722a4d4ddec712ebdc02f1011755059a5e6f1769
Vikk_Moor <victimofuroboros@gmail.com>
2023-01-09 18:26:48 +0500

    TaskReady

--- final/Program.cs
+++ final/Program.cs
@@ -1,3 +1,33 @@
-﻿
+﻿void PrintArray(string[] arr1)
+{
+    int len = arr1.Length;
+    int pos = 0;
+    while (pos < len)
+    {
+        Console.Write($"{arr1[pos]} ");
+        pos++;
+    }
+}
 
+void Task(string[] arr2, string[] arr3)
+{
+    int len3 = 0;
+    for (int i = 0; i < arr2.Length; i++)
+    {
+        if (arr2[i].Length <= 3)
+        {
+            arr3[len3] = arr2[i];
+            len3++;
+        }
+    }
+}
 
+Console.Write("Enter text elements separated by spaces: ");
+string text = Console.ReadLine();
+string[] array = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+string[] array1 = new string[array.Length];
+
+PrintArray(array);
+Console.WriteLine();
+Task(array, array1);
+PrintArray(array1);
